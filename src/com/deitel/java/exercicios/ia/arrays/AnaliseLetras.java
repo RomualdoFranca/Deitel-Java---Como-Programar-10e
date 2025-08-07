0920c045d9e432340a6ec7bbcff76bedb7753c67
RomualdoFranca <romualdo.ads2022@gmail.com>
2025-08-07 20:42:00 -0300

    implementacoes: array de caracteres para armazenar as letras da frase loop para iterar sobre os caracteres da frase e adicionar na array de caracteres Insere alguns println para depuracao, como verrificar o tamanho e tipo

--- src/com/deitel/java/exercicios/ia/arrays/AnaliseLetras.java
+++ src/com/deitel/java/exercicios/ia/arrays/AnaliseLetras.java
@@ -12,12 +12,32 @@ public class AnaliseLetras {
 		Scanner sc =  new Scanner(System.in);
 		
 		System.out.println("Digite uma frase:");
-		//converte as letras da frase em minuscula e retira os espaços
+		
+		//formata as letras da frase em minuscula e retira os espaços
 		String frase = sc.nextLine().toLowerCase().trim();
+		//tamanho da frase inseridda
+		System.out.println("Número de caracteres da frase sem espaços:\n" + frase.length());
+		
+		//array para armazenas os caracteres da frase
+		//o tamanho da array vai ser definido pelo numero de caracteres da frase
+		char[] fraseCaracteres = new char[frase.length()];
+		
+		//iterando sobre as letras da frase
+		for (int i = 0; i < frase.length(); i++) {
+			//em cada posicao da array de caracteres, vai ser adicionado a letra da frase correspondente a mesma posicao
+			fraseCaracteres[i] = frase.charAt(i);
+		}
+		System.out.println("Imprimindo o último caractére: " + fraseCaracteres[4]);
+		System.out.println("Tamanho da array de caracteres: " + fraseCaracteres.length);
+		System.out.println(fraseCaracteres.getClass().isArray());
+		
+		for (char c : fraseCaracteres) {
+			System.out.print(c + " ");
+		}
 
 		String[] frequenciaLetras = new String[] {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l",
 				"m", "n", "o", "p", "k", "r", "s", "t", "u", "v", "x", "y", "w", "z"};
-		System.out.println(frequenciaLetras.length);
+//		System.out.println(frequenciaLetras.length);
 		
 		
 	
