a4c6b0ee8e7e61c54d6d8bae0b4eb32f222830bd
RomualdoFranca <romualdo.ads2022@gmail.com>
2025-08-15 11:12:37 -0300

    ...

--- src/com/deitel/java/exercicios/ia/arrays/SorteioLoteria.java
+++ src/com/deitel/java/exercicios/ia/arrays/SorteioLoteria.java
@@ -11,11 +11,12 @@ public class SorteioLoteria {
 		int min = 1;
 		int max = 60;
 
-		int[] frequenciaNumerica = new int[61];// array que armazena a frequencia em que os numeros aparecem
+		//tamanho da array é de 61 para abranger os números de 1 a 60, pois o índice 0 não será usado.
+		int[] frequenciaNumerica = new int[61];
 		int numeroSorteado = 0;
 		
 		for (int i = 1; i <= 10; i++) {//o loop externo realiza os 10 sorteios
-			for (int j = 1; j<= 6; j++) {
+			for (int j = 1; j<= 6; j++) {//o loop interno realiza o sorteio dos 6 numeros
 				numeroSorteado = (int) (Math.random() * (max - min + 1) + min);
 				++frequenciaNumerica[numeroSorteado];
 				System.out.print(numeroSorteado + " ");
@@ -27,6 +28,14 @@ public class SorteioLoteria {
 		System.out.printf("\n%s%15s" , "Números do Sorteio","Frequência");
 		for (int i = 0; i < frequenciaNumerica.length; i++) {
 			System.out.printf("\n%10d%18d%n" ,i, frequenciaNumerica[i]);
+		}
+		
+		System.out.println("Foreach");
+		
+		System.out.printf("\n%s%15s" , "Números do Sorteio","Frequência");
+		for (int i : frequenciaNumerica) {
+			System.out.printf("\n%10d%18d%n" ,i, frequenciaNumerica[i]);
+			
 		}
 
 	}
